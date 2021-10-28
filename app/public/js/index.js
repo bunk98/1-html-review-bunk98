9a749cc2da5a6fd204c5e9883d37639225c26bc4
bunk98 <bunk98@gmail.com>
2021-10-28 14:36:17 -0400

    edit and delete actually working

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,6 +1,7 @@
 const Offer = {
     data() {
       return {
+            "person": {},
             "books": [],
             "students": [],
             "offers": [],
@@ -10,9 +11,26 @@ const Offer = {
         }
     },
     computed: {
-       
+      prettyBirthday() {
+        return dayjs(this.person.dob.date)
+        .format('D MMM YYYY')
+        }
     },
     methods: {
+      fetchUserData() {
+        console.log("A");
+        fetch('https://randomuser.me/api/')
+        .then( response => response.json() )
+        .then( (responseJson) => {
+            console.log(responseJson);
+            console.log("C");
+            this.person = responseJson.results[0];
+        })
+        .catch( (err) => {
+            console.error(err);
+        })
+        console.log("B");
+    },
         
         fetchBookData() {
             fetch('/api/books/')
@@ -119,6 +137,21 @@ const Offer = {
     },
     created() {
         this.fetchBookData();
+
+        console.log("A");
+        fetch('https://randomuser.me/api/')
+        .then( response => response.json() )
+        .then( (responseJson) => {
+            console.log(responseJson);
+            console.log("C");
+            this.person = responseJson.results[0];
+        })
+        .catch( (err) => {
+            console.error(err);
+        })
+        console.log("B");
+        this.fetchUserData();
+        
     }
   }
   
